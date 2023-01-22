1ad67790b46d54ab1c22c1c96c6a20315dc27744
Furs Andrei <you@example.com>
2023-01-22 18:11:41 +0300

    add category tests

---
+++ tests/Feature/CategoryTest.php
@@ -0,0 +1,113 @@
+<?php
+
+namespace Tests\Feature;
+
+use App\Models\Category;
+use Illuminate\Foundation\Testing\RefreshDatabase;
+use Illuminate\Foundation\Testing\WithFaker;
+use Tests\TestCase;
+
+class CategoryTest extends TestCase
+{
+    use RefreshDatabase;
+
+    public function test_index_page_status_200()
+    {
+        $response = $this->get('/api/categories');
+
+        $response->assertStatus(200);
+    }
+
+    public function test_index_page_json_without_data()
+    {
+        $response = $this->get('/api/categories');
+
+        $response->assertJsonPath('data', []);
+    }
+
+    public function test_index_page_json_with_data()
+    {
+        $category = Category::create(
+            [
+                'name' => 'cat',
+            ]
+        );
+
+        $response = $this->get('/api/categories');
+        
+        $response->assertJsonFragment(
+            [
+                'id' => $category->id,
+                'name' => $category->name,
+                'products' => [],
+            ]
+        );
+    }
+    
+    public function test_show_page_status_200()
+    {
+        $category = Category::create(
+            [
+                'name' => 'cat',
+            ]
+        );
+
+        $response = $this->get('/api/categories/'.$category->id);
+
+        $response->assertStatus(200);
+    }
+
+    public function test_show_page_json_data()
+    {
+        $category = Category::create(
+            [
+                'name' => 'cat',
+            ]
+        );
+
+        $response = $this->get('/api/categories/'.$category->id);
+
+        $response->assertJsonPath('name', $category->name);
+    }
+
+    public function test_store()
+    {
+        $category = [
+            'name' => 'cat',
+        ];
+        $response = $this->post('/api/categories', $category);
+
+        $this->assertDatabaseCount('categories', 1);
+        $this->assertDatabaseHas('categories', $category);
+    }
+
+    public function test_destroy()
+    {
+        $category = Category::create(
+            [
+                'name' => 'cat',
+            ]
+        );
+        
+        $this->assertDatabaseHas('categories', ['id' => $category->id]);
+        $this->delete('/api/categories/'.$category->id);
+        $this->assertDatabaseMissing('categories', ['id' => $category->id]);
+    }
+
+    public function test_update()
+    {
+        $oldCategory = [
+            'name' => 'cat',
+        ];
+        $category = Category::create($oldCategory);
+        $this->assertDatabaseHas('categories', ['name' => 'cat']);
+
+        $newCategory = [
+            'name' => 'new cat',
+        ];
+        $this->put('/api/categories/'.$category->id, $newCategory);
+        
+        $this->assertDatabaseMissing('categories', $oldCategory);
+        $this->assertDatabaseHas('categories', $newCategory);
+    }
+}
