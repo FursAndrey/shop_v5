f43b5f7fe5aed520bbe7c48677573bf665cceaca
Furs Andrei <you@example.com>
2023-02-23 18:26:02 +0300

    refactoring product test

--- tests/Feature/ProductTest.php
+++ tests/Feature/ProductTest.php
@@ -2,17 +2,11 @@
 
 namespace Tests\Feature;
 
-use App\Actions\TestingActions\Create\CreateTestCategoryAction;
 use App\Actions\TestingActions\Create\CreateTestOptionAction;
-use App\Actions\TestingActions\Create\CreateTestProductAction;
-use App\Actions\TestingActions\Create\CreateTestPropertyAction;
-use App\Actions\TestingActions\Create\CreateTestProductPropertyRelationAction;
 use App\Actions\TestingActions\Create\CreateTestSkuAction;
-use App\Actions\TestingActions\Get\GetTestCategoryAction;
 use App\Actions\TestingActions\Get\GetTestOptionAction;
-use App\Actions\TestingActions\Get\GetTestProductAction;
-use App\Actions\TestingActions\Get\GetTestPropertyAction;
 use App\Actions\TestingActions\Get\GetTestSkuWithoutImageAction;
+use App\Actions\TestingActions\Prepare\PrepareTestProductAction;
 use Illuminate\Foundation\Testing\RefreshDatabase;
 use Illuminate\Foundation\Testing\WithFaker;
 use Tests\TestCase;
@@ -37,151 +31,64 @@ class ProductTest extends TestCase
 
     public function test_index_page_json_with_data()
     {
-        $property = (new CreateTestPropertyAction)(
-            (new GetTestPropertyAction)()
-        );
-        $category = (new CreateTestCategoryAction)(
-            (new GetTestCategoryAction)()
-        );
-        $product = (new CreateTestProductAction)(
-            (new GetTestProductAction)($property->id, $category->id)
-        );
-        (new CreateTestProductPropertyRelationAction)($property->id, $product->id);
+        $product = (new PrepareTestProductAction)->full();
 
         $response = $this->get('/api/products');
 
-        $response->assertJsonFragment(
-            [
-                'id' => $product->id,
-                'name' => $product->name,
-                'description' => $product->description,
-                'category' => [
-                    'id' => $category->id,
-                    'name' => $category->name,
-                ],
-                'properties' => [
-                    [
-                        'id' => $property->id,
-                        'name' => $property->name,
-                    ]
-                ],
-            ]
-        );
+        $response->assertJsonFragment($product);
     }
 
     public function test_product_all_page_json_with_data()
     {
-        $property = (new CreateTestPropertyAction)(
-            (new GetTestPropertyAction)()
-        );
-        $category = (new CreateTestCategoryAction)(
-            (new GetTestCategoryAction)()
-        );
-        $product1 = (new CreateTestProductAction)(
-            (new GetTestProductAction)($property->id, $category->id)
-        );
-        $product2 = (new CreateTestProductAction)(
-            (new GetTestProductAction)($property->id, $category->id)
-        );
+        $product1 = (new PrepareTestProductAction)->short();
+        $product2 = (new PrepareTestProductAction)->short();
 
         $response = $this->get('/api/product/all');
 
         $response->assertExactJson(
             [
-                [
-                    'id' => $product1->id,
-                    'name' => $product1->name,
-                    'description' => $product1->description,
-                ],
-                [
-                    'id' => $product2->id,
-                    'name' => $product2->name,
-                    'description' => $product2->description,
-                ]
+                $product1,
+                $product2,
             ]
         );
     }
 
     public function test_show_page_status_200()
     {
-        $property = (new CreateTestPropertyAction)(
-            (new GetTestPropertyAction)()
-        );
-        $option = (new CreateTestOptionAction)(
-            (new GetTestOptionAction)($property->id)
-        );
-        $category = (new CreateTestCategoryAction)(
-            (new GetTestCategoryAction)()
-        );
-        $product = (new CreateTestProductAction)(
-            (new GetTestProductAction)($property->id, $category->id)
-        );
-        $sku = (new CreateTestSkuAction)(
-            (new GetTestSkuWithoutImageAction)($product->id, $option->id)
-        );
+        $product = (new PrepareTestProductAction)->short();
 
-        $response = $this->get('/api/products/'.$product->id);
+        $response = $this->get('/api/products/'.$product['id']);
 
         $response->assertStatus(200);
     }
 
     public function test_show_page_json_data()
     {
-        $property = (new CreateTestPropertyAction)(
-            (new GetTestPropertyAction)()
-        );
+        $product = (new PrepareTestProductAction)->full();
+
         $option = (new CreateTestOptionAction)(
-            (new GetTestOptionAction)($property->id)
+            (new GetTestOptionAction)($product['properties'][0]['id'])
         );
-        $category = (new CreateTestCategoryAction)(
-            (new GetTestCategoryAction)()
-        );
-        $product = (new CreateTestProductAction)(
-            (new GetTestProductAction)($property->id, $category->id)
-        );
-        (new CreateTestProductPropertyRelationAction)($property->id, $product->id);
         $sku = (new CreateTestSkuAction)(
-            (new GetTestSkuWithoutImageAction)($product->id, $option->id)
+            (new GetTestSkuWithoutImageAction)($product['id'], $option->id)
         );
 
-        $response = $this->get('/api/products/'.$product->id);
+        $response = $this->get('/api/products/'.$product['id']);
 
-        $response->assertJsonFragment(
+        $product['skus'] = [
             [
-                'id' => $product->id,
-                'name' => $product->name,
-                'description' => $product->description,
-                'category' => [
-                    'id' => $category->id,
-                    'name' => $category->name,
-                ],
-                'properties' => [
-                    [
-                        'id' => $property->id,
-                        'name' => $property->name,
-                    ]
-                ],
-                'skus' => [
-                    [
-                        'id' => $sku->id,
-                        'count' => $sku->count,
-                        'price' => $sku->price,
-                        'options' => []
-                    ]
-                ]
+                'id' => $sku->id,
+                'count' => $sku->count,
+                'price' => $sku->price,
+                'options' => []
             ]
-        );
+        ];
+        $response->assertJsonFragment($product);
     }
 
     public function test_store()
     {
-        $property = (new CreateTestPropertyAction)(
-            (new GetTestPropertyAction)()
-        );
-        $category = (new CreateTestCategoryAction)(
-            (new GetTestCategoryAction)()
-        );
-        $product = (new GetTestProductAction)($property->id, $category->id);
+        $product = (new PrepareTestProductAction)->noDB();
 
         $this->assertDatabaseCount('products', 0);
         $this->post('/api/products', $product);
@@ -191,62 +98,45 @@ class ProductTest extends TestCase
         unset($product['property_id']);
 
         $this->assertDatabaseHas('products', $product);
-        $this->assertDatabaseHas(
-            'product_property',
-            [
-                'property_id' => $property->id,
-            ]
-        );
+        //продумать необходимость этой проверки
+        // $this->assertDatabaseHas(
+        //     'product_property',
+        //     [
+        //         'property_id' => $property->id,
+        //     ]
+        // );
     }
 
     public function test_destroy()
     {
-        $property = (new CreateTestPropertyAction)(
-            (new GetTestPropertyAction)()
-        );
-        $category = (new CreateTestCategoryAction)(
-            (new GetTestCategoryAction)()
-        );
-        $product = (new CreateTestProductAction)(
-            (new GetTestProductAction)($property->id, $category->id)
-        );
-        (new CreateTestProductPropertyRelationAction)($property->id, $product->id);
+        $product = (new PrepareTestProductAction)->short();
 
-        $this->assertDatabaseHas('products', ['id' => $product->id]);
-        $this->delete('/api/products/'.$product->id);
-        $this->assertDatabaseMissing('products', ['id' => $product->id]);
+        $this->assertDatabaseHas('products', $product);
+        $this->delete('/api/products/'.$product['id']);
+        $this->assertDatabaseMissing('products', $product);
     }
 
     public function test_update()
     {
-        $property = (new CreateTestPropertyAction)(
-            (new GetTestPropertyAction)()
-        );
-        $category = (new CreateTestCategoryAction)(
-            (new GetTestCategoryAction)()
-        );
-
-        $oldProduct = (new GetTestProductAction)($property->id, $category->id);
-        $product = (new CreateTestProductAction)($oldProduct);
-
-        (new CreateTestProductPropertyRelationAction)($property->id, $product->id);
+        $oldProduct = (new PrepareTestProductAction)->short();
 
         $this->assertDatabaseHas('products', ['name' => $oldProduct['name']]);
 
-        $newProduct = (new GetTestProductAction)($property->id, $category->id);
-        $this->put('/api/products/'.$product->id, $newProduct);
+        $newProduct = (new PrepareTestProductAction)->noDB();
+        $this->put('/api/products/'.$oldProduct['id'], $newProduct);
 
         unset($oldProduct['property_id']);
         unset($newProduct['property_id']);
 
         $this->assertDatabaseMissing('products', $oldProduct);
         $this->assertDatabaseHas('products', $newProduct);
-        $this->assertDatabaseHas(
-            'product_property',
-            [
-                'product_id' => $product->id,
-                'property_id' => $property->id,
-            ]
-        );
+        //продумать необходимость этой проверки
+        // $this->assertDatabaseHas(
+        //     'product_property',
+        //     [
+        //         'product_id' => $product->id,
+        //         'property_id' => $property->id,
+        //     ]
+        // );
     }
 }
